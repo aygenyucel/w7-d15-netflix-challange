9312bb377a5fd60c21ff866d095ca530a6c115e9
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-11 17:49:51 +0300

    updated functions for fetching and display

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-async function getHorrorMovies() {
+async function getMovies(category) {
   const response = await fetch(
-    "https://striveschool-api.herokuapp.com/api/movies/horror",
+    `https://striveschool-api.herokuapp.com/api/movies/${category}`,
     {
       headers: {
         Authorization:
@@ -8,45 +8,30 @@ async function getHorrorMovies() {
       },
     }
   );
-  const horrorMovies = await response.json();
-  console.log(horrorMovies);
-  return horrorMovies;
+  const movies = await response.json();
+  console.log(movies);
+  return movies;
 }
 
-async function getRomanticMovies() {
-  const response = await fetch(
-    "https://striveschool-api.herokuapp.com/api/movies/romantic",
-    {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZmI2NWQ0YmUzZDAwMTU4NDYwMmIiLCJpYXQiOjE2NjgwODY2MjksImV4cCI6MTY2OTI5NjIyOX0.TamAcG1oYuQj5l9U-hIKdU_vmPqKfceiGpckjYXJ2OY",
-      },
-    }
-  );
-  const romanticMovies = await response.json();
-  console.log(romanticMovies);
-  return romanticMovies;
-}
-
-function renderHorrorMovies(listOfMovies) {
-  let horrorMovieRow = document.querySelector("#horror-movie-row");
+function renderMovies(listOfMovies, category) {
+  let movieRow = document.querySelector(`#${category}-movie-row`);
 
   listOfMovies.forEach((movie) => {
-    console.log("aa");
-    const horrorMovieCol = document.createElement("div");
-    horrorMovieCol.classList.add("col-md-2");
-    horrorMovieCol.innerHTML = `<img
+    const movieCol = document.createElement("div");
+    movieCol.classList.add("col-md-2");
+    movieCol.innerHTML = `<img
                                     class="movie-cover"
-                                    id="horror-movie-cover"
+                                    id="${category}-movie-cover"
                                     src=${movie.imageUrl}
                                     />`;
 
-    horrorMovieRow.appendChild(horrorMovieCol);
+    movieRow.appendChild(movieCol);
   });
 }
 
 window.onload = async () => {
-  console.log("xxxx");
-  const movies = await getHorrorMovies();
-  renderHorrorMovies(movies);
+  const horrorMovies = await getMovies("horror");
+  const romanticMovies = await getMovies("romantic");
+  renderMovies(horrorMovies, "horror");
+  renderMovies(romanticMovies, "romantic");
 };
